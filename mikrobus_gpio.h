6969f941d447c895305267c6f065eaaeeb50b42b
vaishnav98 <mavaishnav007@gmail.com>
2019-07-29 22:28:34 +0000

    add simple module to export mikrobus greybus gpio

---
+++ mikrobus_gpio.h
@@ -0,0 +1,22 @@
+/*
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __LINUX_MIKROBUS_GPIO_H
+#define __LINUX_MIKROBUS_GPIO_H
+
+#include <linux/gpio.h>
+
+#define MIKROBUS_NUM_GPIOS 6
+#define MIKROBUS_START_GPIO 506
+
+#endif /* __LINUX_MIKROBUS_GPIO_H */
